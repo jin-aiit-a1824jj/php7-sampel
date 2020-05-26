f6e6770f16e1ff0657e24f1b627ab97436b009a6
JIN <a1824jj@aiit.ac.jp>
2020-05-26 07:27:14 +0000

    print

--- session2/sample01.php
+++ session2/sample01.php
@@ -1,10 +1,16 @@
 <!DOCTYPE html>
-<html lang="en">
+
+<html lang="ja">
+
 <head>
   <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  
   <link rel="stylesheet" href="css/style.css">
+  
   <title>PHP</title>
 </head>
+
 <body>
   <header>
     <h1 class="font-weight-normal">PHP</h1>
@@ -13,8 +19,10 @@
   <main>
     <h2>Practice</h2>
     <pre>
-      <? php
-        print('PHPを勉強中です！');
+      <?php
+        print("こんにちは\n");
+        print("Hello PHP7\n");
+        print('PHPを勉強中です。');
       ?>
     </pre>
   </main>
