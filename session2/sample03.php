196d08893a9cbc21523075978c7492e2291ce51c
JIN <a1824jj@aiit.ac.jp>
2020-05-26 07:38:47 +0000

    get now time

---
+++ session2/sample03.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+
+<html lang="ja">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  
+  <link rel="stylesheet" href="css/style.css">
+  
+  <title>PHP</title>
+</head>
+
+<body>
+  <header>
+    <h1 class="font-weight-normal">PHP</h1>
+  </header>
+
+  <main>
+    <h2>Practice</h2>
+    <pre>
+      <?php
+        date_default_timezone_set('Asia/Tokyo');
+        print(date('G時　i分　s秒'));
+        print(date('l'));
+      ?>
+    </pre>
+  </main>
+
+</body>
+</html>
