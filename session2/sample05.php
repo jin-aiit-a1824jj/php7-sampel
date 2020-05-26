e9a9669d28f528bf70790513ef8c50f076d4c944
JIN <a1824jj@aiit.ac.jp>
2020-05-26 07:55:23 +0000

    using variable

---
+++ session2/sample05.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+
+<html lang="ja">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  
+  <link rel="stylesheet" href="css/style.css">
+  
+  <title>PHP</title>
+</head>
+
+<body>
+  <header>
+    <h1 class="font-weight-normal">PHP</h1>
+  </header>
+
+  <main>
+    <h2>Practice</h2>
+    <pre>    
+      <?php 
+        $sum = 100+1050+200;
+        $tax = 1.10;
+      ?>
+      合計金額は：<?php print($sum); ?>円です。
+      税込み価格は：<?php print($sum*$tax); ?>円です。
+    </pre>
+  </main>
+
+</body>
+</html>
