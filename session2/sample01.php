3e1c3e1897c1499a7ebe2fd74174e78f61183644
JIN <a1824jj@aiit.ac.jp>
2020-05-26 07:14:21 +0000

    install apache2 & config

---
+++ session2/sample01.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <link rel="stylesheet" href="css/style.css">
+  <title>PHP</title>
+</head>
+<body>
+  <header>
+    <h1 class="font-weight-normal">PHP</h1>
+  </header>
+
+  <main>
+    <h2>Practice</h2>
+    <pre>
+      <? php
+        print('PHPを勉強中です！');
+      ?>
+    </pre>
+  </main>
+
+</body>
+</html>
