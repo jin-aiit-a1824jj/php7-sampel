c1a452fd8aaa90d057bafcc04ceb058960fa2ada
JIN <a1824jj@aiit.ac.jp>
2020-05-27 03:20:16 +0000

    cookie - sample

---
+++ session2/sample24/index.php
@@ -0,0 +1,32 @@
+<?php 
+  setcookie('save_message', 'Cookieに保存した値です。', time() + 60 * 60 * 24 * 14);
+?>
+
+<!DOCTYPE html>
+
+<html lang="ja">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  
+  <link rel="stylesheet" href="../css/style.css">
+  
+  <title>PHP</title>
+</head>
+
+<body>
+  <header>
+    <h1 class="font-weight-normal">PHP</h1>
+  </header>
+
+  <main>
+    <h2>Practice</h2>
+    <pre>    
+      <a href="page02.php">Page02へ</a>
+    </pre>
+    
+  </main>
+
+</body>
+</html>
