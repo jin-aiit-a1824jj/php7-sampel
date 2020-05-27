18b1a6ae789d1bafd818b7031784cc054d0decc8
JIN <a1824jj@aiit.ac.jp>
2020-05-27 02:50:58 +0000

    mb_convert_kana - sample

---
+++ session2/sample20.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+
+<html lang="ja">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  
+  <link rel="stylesheet" href="css/style.css">
+  
+  <title>PHP</title>
+</head>
+
+<body>
+  <header>
+    <h1 class="font-weight-normal">PHP</h1>
+  </header>
+
+  <main>
+    <h2>Practice</h2>
+    <pre>    
+
+      <?php
+        $age = '２０';
+        $age = mb_convert_kana($age, 'n', 'UTF-8');
+        if (is_numeric($age)) {
+          print($age.'歳');
+        } else {
+          print('※　年齢が数字ではありません');
+        }
+      ?>
+     
+    </pre>
+    
+  </main>
+
+</body>
+</html>
