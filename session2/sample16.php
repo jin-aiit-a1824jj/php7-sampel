1c071b07d74dd7e1d6e78a0ae1692f0021e06e25
JIN <a1824jj@aiit.ac.jp>
2020-05-27 02:03:21 +0000

    json_decode - sample

---
+++ session2/sample16.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+
+<html lang="ja">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  
+  <link rel="stylesheet" href="css/style.css">
+  
+  <title>PHP</title>
+</head>
+
+<body>
+  <header>
+    <h1 class="font-weight-normal">PHP</h1>
+  </header>
+
+  <main>
+    <h2>Practice</h2>
+    <pre>    
+      <?php
+        $file = file_get_contents('https://h2o-space.com/feed/json/');
+        $json = json_decode($file);
+        //var_dump($json);
+        foreach($json->items as $item):
+      ?>
+      <li><a href="<?php print($item->url); ?>"><?php print($item->title) ?></a></li>
+      <?php endforeach;?> 
+    </pre>
+  </main>
+
+</body>
+</html>
