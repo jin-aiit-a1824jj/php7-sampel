0f37798f65d744bb310642753908cf5fac8e931a
JIN <a1824jj@aiit.ac.jp>
2020-05-28 02:07:41 +0000

    PDO - access mysql

---
+++ session4/index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+
+<html lang="ja">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  
+  <link rel="stylesheet" href="css/style.css">
+  
+  <title>PHP</title>
+</head>
+
+<body>
+  <header>
+    <h1 class="font-weight-normal">PHP</h1>
+  </header>
+
+  <main>
+    <h2>Practice</h2>
+    <pre>
+      <?php
+        try {
+          $db = new PDO('mysql:dbname=mydb;host=127.0.0.1;charset=utf8','mydbuser', 'PassW0rd');
+          
+        } catch(PDOException $e) {
+          echo 'DB接続エラー：'.$e->getMessage();
+        }
+      ?>
+    </pre>
+  </main>
+
+</body>
+</html>
