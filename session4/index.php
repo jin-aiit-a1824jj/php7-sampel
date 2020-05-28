d3312d04d6ac41ee5ba562e1f3abf09aae5e1236
JIN <a1824jj@aiit.ac.jp>
2020-05-28 02:27:12 +0000

    query - excute Select sql

--- session4/index.php
+++ session4/index.php
@@ -27,8 +27,14 @@
           echo 'DB接続エラー：'.$e->getMessage();
         }
 
-        $count = $db->exec('INSERT INTO my_items SET maker_id=1, item_name="もも", price=210, keyword="缶詰、ビンク、甘い", sales=0 ');
-        echo $count.'件のデータを挿入しました'; 
+        // $count = $db->exec('INSERT INTO my_items SET maker_id=1, item_name="もも", price=210, keyword="缶詰、ビンク、甘い", sales=0 ');
+        // echo $count.'件のデータを挿入しました'; 
+
+        $records = $db->query('SELECT * FROM my_items');
+        while($record = $records->fetch()){
+          print($record['item_name']."\n");
+        }
+
         var_dump($db->errorInfo());
       ?>
     </pre>
